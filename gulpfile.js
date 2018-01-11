5cad267fb2cc3690ac3d19b790155d7e2821e537
TAKTY-MBP-ST\Takty <tokutadaginaya@gmail.com>
2018-01-12 00:45:44 +0900

    Add settings for using babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
-var gulp     = require('gulp');
-var plumber  = require('gulp-plumber');
-var uglify   = require('gulp-uglify');
-var rename   = require('gulp-rename');
+var gulp    = require('gulp');
+var plumber = require('gulp-plumber');
+var uglify  = require('gulp-uglify');
+var rename  = require('gulp-rename');
+var babel   = require('gulp-babel');
 
 gulp.task('js', function() {
 	gulp.src('src/slide-show/**/*.js')
 	.pipe(plumber())
+	.pipe(babel({presets: ['es2015']}))
 	.pipe(uglify())
 	.pipe(rename({extname: '.min.js'}))
 	.pipe(gulp.dest('dist/slide-show'));
 
 	gulp.src('src/background-image/**/*.js')
 	.pipe(plumber())
+	.pipe(babel({presets: ['es2015']}))
 	.pipe(uglify())
 	.pipe(rename({extname: '.min.js'}))
 	.pipe(gulp.dest('dist/background-image'));
