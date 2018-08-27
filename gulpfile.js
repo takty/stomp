de63b73dfa8ce9b71cee1ddf769080b675c9e860
Takty <tokutadaginaya@gmail.com>
2018-08-27 11:45:54 +0900

    Update packages and gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,46 @@
-var gulp    = require('gulp');
-var plumber = require('gulp-plumber');
-var uglify  = require('gulp-uglify');
-var rename  = require('gulp-rename');
-var babel   = require('gulp-babel');
+'use strict';
 
-gulp.task('js', function() {
-	gulp.src('src/slide-show/**/*.js')
-	.pipe(plumber())
-	.pipe(babel({presets: ['es2015']}))
-	.pipe(uglify())
-	.pipe(rename({extname: '.min.js'}))
-	.pipe(gulp.dest('dist/slide-show'));
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')({ pattern: ['gulp-*'] });
 
-	gulp.src('src/background-image/**/*.js')
-	.pipe(plumber())
-	.pipe(babel({presets: ['es2015']}))
-	.pipe(uglify())
-	.pipe(rename({extname: '.min.js'}))
-	.pipe(gulp.dest('dist/background-image'));
+
+gulp.task('js-slide-show', () => {
+	return gulp.src('src/slide-show/**/*.js')
+		.pipe($.plumber())
+		.pipe($.babel({ presets: [['env', { targets: { ie: 11 } }]] }))
+		.pipe($.uglify())
+		.pipe($.rename({ extname: '.min.js' }))
+		.pipe(gulp.dest('dist/slide-show'));
 });
 
-gulp.task('sass', function () {
-	gulp.src(['src/slide-show/**/*.scss'], {base: 'src/slide-show'})
-	.pipe(gulp.dest('dist/slide-show'));
-
-	gulp.src(['src/background-image/**/*.scss'], {base: 'src/background-image'})
-	.pipe(gulp.dest('dist/background-image'));
+gulp.task('js-background-image', () => {
+	return gulp.src('src/background-image/**/*.js')
+		.pipe($.plumber())
+		.pipe($.babel({ presets: [['env', { targets: { ie: 11 } }]] }))
+		.pipe($.uglify())
+		.pipe($.rename({ extname: '.min.js' }))
+		.pipe(gulp.dest('dist/background-image'));
 });
 
-gulp.task('watch', function() {
-	gulp.watch('src/**/*.js', ['js']);
-	gulp.watch('src/**/*.scss', ['sass']);
+gulp.task('js', gulp.parallel('js-slide-show', 'js-background-image'));
+
+gulp.task('sass-slide-show', () => {
+	return gulp.src(['src/slide-show/**/*.scss'], { base: 'src/slide-show' })
+		.pipe(gulp.dest('dist/slide-show'));
 });
 
-gulp.task('build', ['js', 'sass']);
-gulp.task('default', ['js', 'sass', 'watch']);
+gulp.task('sass-background-image', () => {
+	return gulp.src(['src/background-image/**/*.scss'], { base: 'src/background-image' })
+		.pipe(gulp.dest('dist/background-image'));
+});
+
+gulp.task('sass', gulp.parallel('sass-slide-show', 'sass-background-image'));
+
+gulp.task('watch', () => {
+	gulp.watch('src/**/*.js', gulp.series('js'));
+	gulp.watch('src/**/*.scss', gulp.series('sass'));
+});
+
+gulp.task('build', gulp.parallel('js', 'sass'));
+
+gulp.task('default', gulp.series('build', 'watch'));
