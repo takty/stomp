344b01220e434a3d04875a529fd14722a57bbbfb
Takty <tokutadaginaya@gmail.com>
2018-10-27 14:09:30 +0900

    Add the document

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path = require('path');
 const gulp = require('gulp');
 const $ = require('gulp-load-plugins')({ pattern: ['gulp-*'] });
 
@@ -44,3 +45,48 @@ gulp.task('watch', () => {
 gulp.task('build', gulp.parallel('js', 'sass'));
 
 gulp.task('default', gulp.series('build', 'watch'));
+
+
+// -----------------------------------------------------------------------------
+
+gulp.task('docs-lib', () => {
+	return gulp.src(['node_modules/{stile,stool}/dist/**/*'])
+		.pipe($.plumber())
+		.pipe($.rename((p) => {
+			p.dirname = p.dirname.replace(path.sep + 'dist', '');
+		}))
+		.pipe(gulp.dest('./'));
+});
+
+gulp.task('docs-sass', () => {
+	return gulp.src('docs/style.scss')
+		.pipe($.plumber())
+		.pipe($.sourcemaps.init())
+		.pipe($.sass({outputStyle: 'compressed'}))
+		.pipe($.autoprefixer({browsers: ['ie >= 11'], remove: false}))
+		.pipe($.rename({extname: '.min.css'}))
+		.pipe($.sourcemaps.write('.'))
+		.pipe(gulp.dest('docs'));
+});
+
+gulp.task('docs-js', gulp.series('js', () => {
+	return gulp.src([
+			'node_modules/stile/dist/js/stile-full.min.js',
+			'dist/background-image/background-image.min.js',
+			'dist/slide-show/slide-show.min.js'
+		])
+		.pipe($.plumber())
+		.pipe(gulp.dest('docs'));
+}));
+
+gulp.task('docs-watch', () => {
+	gulp.watch('src/js/**/*.js',     gulp.series('docs-js'));
+	gulp.watch('src/sass/**/*.scss', gulp.series('docs-sass'));
+	gulp.watch('docs/style.scss',    gulp.series('docs-sass'));
+});
+
+gulp.task('docs-build', gulp.parallel('docs-js', 'docs-sass'));
+
+gulp.task('docs-default', gulp.series('docs-build', 'docs-watch'));
+
+gulp.task('docs', gulp.parallel('default', 'docs-default'));
